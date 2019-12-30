f37b49465bbba485c180715d63d7ce1c3624808e
heberqc <heberqc@gmail.com>
2019-12-30 15:30:48 -0500

    load avatar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,33 @@ import { debounceTime } from 'rxjs/operators';
 })
 export class AppComponent  {
   @ViewChild('nickname', { static: true }) nickname: ElementRef;
+  @ViewChild('avatar', { static: true }) avatar: ElementRef;
+
   input$;
   subscription;
   observer;
 
   constructor() {
     this.observer = {
-      next: evt => console.log(evt.target.value),
+      next: evt => {
+        const nickname = evt.target.value;
+        this.avatar.nativeElement.src = this.imageURI(nickname);
+      },
       error: err => console.error(err),
-      complete: () => console.log(),
+      complete: () => console.log('Fin'),
     }
+  }
+
+  imageURI(nickname) {
+    return `https://api.adorable.io/avatars/300/${nickname}.png`;
   }
 
   ngAfterViewInit() {
     this.input$ = fromEvent(this.nickname.nativeElement, 'keyup').pipe(debounceTime(500));
     this.subscription = this.input$.subscribe(this.observer);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
