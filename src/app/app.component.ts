514f9d71ac6450e144a0c12982f46edd1fe53f84
heberqc <heberqc@gmail.com>
2019-12-30 12:35:13 -0500

    add stream source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -7,5 +8,21 @@ import { fromEvent } from 'rxjs';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  
+  @ViewChild('nickname', { static: true }) nickname: ElementRef;
+  input$;
+  subscription;
+  observer;
+
+  constructor() {
+    this.observer = {
+      next: evt => console.log(evt.target.value),
+      error: err => console.error(err),
+      complete: () => console.log(),
+    }
+  }
+
+  ngAfterViewInit() {
+    this.input$ = fromEvent(this.nickname.nativeElement, 'keyup').pipe(debounceTime(500));
+    this.subscription = this.input$.subscribe(this.observer);
+  }
 }
